171089828e0706b25fcc2bfc1635c0799f99842b
GabrielGustavoDeSouza <gabriel.souza.gustavo@gmail.com>
2026-05-11 17:50:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1251,9 +1251,7 @@ def main():
                             line=dict(color=color, width=2),
                             marker=dict(size=5),
                             customdata=acum_values,
-                            hovertemplate='%{x|%b/%Y}  
-Ganho Mês: <b>R$ %{y:,.0f}</b>  
-Acumulado: <b>R$ %{customdata:,.0f}</b><extra>' + unidade + '</extra>'
+                            hovertemplate='%{x|%b/%Y} Ganho Mês: <b>R$ %{y:,.0f}</b>  Acumulado: <b>R$ %{customdata:,.0f}</b><extra>' + unidade + '</extra>'
                         ))
 
                     # Linha Total Geral (azul mais forte, mais grossa)
@@ -1266,9 +1264,7 @@ Acumulado: <b>R$ %{customdata:,.0f}</b><extra>' + unidade + '</extra>'
                         line=dict(color='#1400FF', width=3.5),
                         marker=dict(size=7),
                         customdata=acum_total,
-                        hovertemplate='%{x|%b/%Y}  
-Ganho Mês: <b>R$ %{y:,.0f}</b>  
-Acumulado: <b>R$ %{customdata:,.0f}</b><extra>Total Geral</extra>'
+                        hovertemplate='%{x|%b/%Y}  Ganho Mês: <b>R$ %{y:,.0f}</b> Acumulado: <b>R$ %{customdata:,.0f}</b><extra>Total Geral</extra>'
                     ))
 
                     fig_tl.update_layout(
